20b7e4a15598653075496141a6a856f971ad1156
richardingham <mail@richardingham.net>
2014-07-05 14:35:18 +0000

    Don't overwrite protoflo.graph

--- protoflo/__init__.py
+++ protoflo/__init__.py
@@ -1,10 +1,10 @@
 
-from components import graph
+from components.graph import Graph as GraphComponent
 
 title = "ProtoFlo"
 name = None
 description = "ProtoFlo Builtin Components"
 
 __components__ = {
-	'Graph': graph.Graph,
+	'Graph': GraphComponent,
 }
